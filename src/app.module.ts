b15e9ebf30ebf2ca40209d50253f7a7da65e9ffd
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-01-11 20:08:43 +0200

    initialising the graphql files

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,21 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ArticlesModule } from './articles/articles.module';
-
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { PostModule } from './post/post.module';
 @Module({
-  imports: [AuthModule, UsersModule, AuthModule, PrismaModule, ArticlesModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    AuthModule,
+    PrismaModule,
+    ArticlesModule,
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+    }),
+    PostModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
